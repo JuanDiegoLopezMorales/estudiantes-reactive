11e3fda32cb2887a08d1596ed558c449fb7a8615
JuanDiegoLopezMorales <lopezjuandiego411@gmail.com>
2025-08-21 08:50:07 -0500

    Update EstudianteRepository.java

--- src/main/java/com/example/estudiantesreactive/repository/EstudianteRepository.java
+++ src/main/java/com/example/estudiantesreactive/repository/EstudianteRepository.java
@@ -1,4 +1,11 @@
 package com.example.estudiantesreactive.repository;
 
-public class EstudianteRepository {
+import com.example.estudiantesreactive.domain.Estudiante;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+
+public interface EstudianteRepository extends ReactiveCrudRepository<Estudiante, Long> {
+    Flux<Estudiante> findByNombreContainingIgnoreCase(String nombre);
+    Flux<Estudiante> findByCarreraIgnoreCase(String carrera);
+    Flux<Estudiante> findBySemestre(Integer semestre);
 }
