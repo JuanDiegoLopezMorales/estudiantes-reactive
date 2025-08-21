e9ac772222ac08e19de0292c05fa52fc6b7fc761
JuanDiegoLopezMorales <lopezjuandiego411@gmail.com>
2025-08-21 08:49:36 -0500

    Update EstudianteRequest.java

--- src/main/java/com/example/estudiantesreactive/dto/EstudianteRequest.java
+++ src/main/java/com/example/estudiantesreactive/dto/EstudianteRequest.java
@@ -1,4 +1,19 @@
 package com.example.estudiantesreactive.dto;
 
-public class EstudianteRequest {
-}
+
+import jakarta.validation.constraints.*;
+
+public record EstudianteRequest(
+        @NotBlank(message = "El nombre es obligatorio")
+        @Size(max = 120, message = "El nombre es demasiado largo")
+        String nombre,
+
+        @NotBlank(message = "La carrera es obligatoria")
+        @Size(max = 100, message = "La carrera es demasiado larga")
+        String carrera,
+
+        @NotNull(message = "El semestre es obligatorio")
+        @Min(value = 1, message = "El semestre mínimo es 1")
+        @Max(value = 20, message = "El semestre máximo permitido es 20")
+        Integer semestre
+) {}
