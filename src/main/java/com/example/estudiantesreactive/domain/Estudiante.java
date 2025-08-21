023f42fb1fbfe24f3d6fa4a1f06bf1b8204574ff
JuanDiegoLopezMorales <lopezjuandiego411@gmail.com>
2025-08-21 08:49:01 -0500

    Update Estudiante.java

--- src/main/java/com/example/estudiantesreactive/domain/Estudiante.java
+++ src/main/java/com/example/estudiantesreactive/domain/Estudiante.java
@@ -1,4 +1,19 @@
 package com.example.estudiantesreactive.domain;
 
-public class Estudiante {
-}
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+@Table("estudiantes")
+public record Estudiante(
+        @Id
+        Long id,
+        String nombre,
+        String carrera,
+        Integer semestre,
+        @Column("created_at")
+        LocalDateTime createdAt
+) {}
